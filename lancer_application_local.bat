cae8a486c217c4b56c4a515033fa6507adce20ba
eliboa <eliboa@gmail.com>
2026-06-01 12:23:23 +0200

    clean project

--- lancer_application_local.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "chromePath=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-set "appPath=file:///%~dp0public/index.html"
-set "userData=%TEMP%\edge_dev_session"
-start "" "%chromePath%" --disable-web-security --user-data-dir="%userData%" --app="%appPath%"
